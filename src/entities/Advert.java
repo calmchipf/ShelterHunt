89e7d1951bf6bef0231f392707ddc4533673c638
Shynggys049 <159523039+Shynggys049@users.noreply.github.com>
2024-02-10 20:14:34 +0600

    Update Advert.java
    Added class Adverts;
    Made constructur, getters and setters for housings id, address, location, price, photo id, description and review id.

--- src/entities/Advert.java
+++ src/entities/Advert.java
@@ -1,4 +1,64 @@
 package entities;
 
-public class Advert {
+public class Adverts {
+    public int id;
+    public string address;
+    public string location;
+    public int price;
+    public int photos_id;
+    public string description;
+    public int review_id;
+
+    public Adverts(int id, string address, string location, int price, int photos_id, string description, int review_id){
+        this.id = id;
+        this.address = address;
+        this.location = location;
+        this.price = price;
+        this.photos_id = photos_id;
+        this.description = description;
+        this.review_id = review_id;
+    }
+    // Getters and setters
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public string getAddress(){
+        return address;
+    }
+    public void setAddress(string address){
+        this.address = address;
+    }
+    public string getLocation(){
+        return location;
+    }
+    public void setLocation(string location){
+        this.location = location;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
+    public int getPhotos_id(){
+        return photos_id;
+    }
+    public void setPhotos_id(int photos_id){
+        this.photos_id = photos_id;
+    }
+    public string getDescription(){
+        return description;
+    }
+    public void setDescription(string description){
+        this.description = description;
+    }
+    public int getReview_id(){
+        return review_id;
+    }
+    public void setReview_id(int review_id){
+        this.review_id = review_id;
+    }
 }
