fa51bfbbcccc15d1931dcbdc6c18eba5b639db54
calmchipf <dk123tm@mail.ru>
2024-02-11 15:36:50 +0600

    Fixed overlapping entities and created UserRepository

---
+++ src/repositories/interfaces/IUserRepository.java
@@ -0,0 +1,15 @@
+package repositories.interfaces;
+
+import entities.Advert;
+import entities.Review;
+import entities.User;
+import java.util.List;
+public interface IUserRepository {
+    boolean createUser(User user);
+    User getUser(int id);
+    List<User> getAllUsers();
+    List<Advert> getOwnedAdverts();
+
+    List<Review> getMadeReviews();
+
+}
