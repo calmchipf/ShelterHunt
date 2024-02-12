165b3beb456455254115a7b84b289345d0915e6c
calmchipf <dk123tm@mail.ru>
2024-02-13 00:26:37 +0600

    Removed unnecessary connection in Main class

--- src/Main.java
+++ src/Main.java
@@ -11,36 +11,10 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        String connectionUrl = "jdbc:postgresql://localhost:5432/ShelterHunt";
-        Connection con = null;
-        ResultSet rs = null;
-        Statement stmt = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-
-            con = DriverManager.getConnection(connectionUrl, "postgres", "ugotitright");
-
-            IDB db = new PostgresDB();
-            IUserRepository repo = new UserRepository(db);
-            UserController controller = new UserController(repo);
-            MyApplication app = new MyApplication(controller);
-            app.start();
-        }
-        catch (Exception e) {
-            System.out.println("Exception occurred!");
-        } finally {
-
-            try { // Close connection - clean up the system resources
-                con.close();
-            } catch (Exception e) {
-                System.out.println("Exception occurred!");
-            }
-        }
-
-        System.out.println("Finished!");
-
-
-
-
+        IDB db = new PostgresDB();
+        IUserRepository repo = new UserRepository(db);
+        UserController controller = new UserController(repo);
+        MyApplication app = new MyApplication(controller);
+        app.start();
     }
 }
