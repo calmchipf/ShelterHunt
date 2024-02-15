6a01b3e894ea9293d17c3cac8ba47724305da1bd
azabr <230147@astanait.edu.kz>
2024-02-15 19:02:49 +0600

    Create RealEstateApp and add constructor

---
+++ src/RealEstateApp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class User {
+    String username;
+    String password;
+    String name;
+    String surname;
+    String dateOfBirth;
+
+    // Constructor
+    public User(String username, String password, String name, String surname, String dateOfBirth) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+    }
+}
+
+
